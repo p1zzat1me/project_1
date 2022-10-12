17553b7147db07d9d4cd8b94e55aa2cb9065a581
niki <2004nnikita@gmail.com>
2022-10-12 23:52:18 +0200

    first shit

---
+++ script.js
@@ -0,0 +1,12 @@
+//let answer = prompt("Are you here?");
+/*console.log(typeof(undefined));
+console.log("arr" + " -object");*/
+let incr = 10;
+let decr = 10;
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+console.log("2"===2);
+let isChecked = true,
+isClosed=false;
+console.log(isChecked || isClosed);
