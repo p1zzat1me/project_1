24a4eb3b72b454c1bb8992c3bf4a572cadae410b
niki <2004nnikita@gmail.com>
2022-10-14 21:24:42 +0200

    homework lesson 3

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let money = +prompt("Ваш бюджет на месяц?",'');
-let time = prompt("Введите дату в формате YYYY-MM-DD",'');
+var money,time;
+function start(){
+  money = +prompt("Ваш бюджет на месяц?",'');
+  time = prompt("Введите дату в формате YYYY-MM-DD",'');
+
+  while(isNaN(money) || money == '' || money == null){
+    money = +prompt("Ваш бюджет на месяц?",'');
+  }
+}
+start();
+
 let appData = {
     budget: money,
     timeData: time,
@@ -8,23 +17,29 @@ let appData = {
     },
     optionalExpenses:{},
     income:[],
-    savings:false
+    savings:true
 };
-for (let i = 0; i < 2;i++){
-var a = prompt("Введите обязательную статью расходов в этом месяце",'');
-var b = prompt("Во сколько обойдется?",'');
-  if ((typeof(a))=== 'string' && (typeof(a)!== 'null' && (typeof(b)!== 'null')
-  && a!= '' && b!= '' && a.length < 50)) {
-    console.log("done");
-    appData.expenses[a] = b;
-  }else{
-   i = -1;
-
-  }
+function chooseExpenses(){
+  for (let i = 0; i < 2;i++){
+    var a = prompt("Введите обязательную статью расходов в этом месяце",'');
+    var b = prompt("Во сколько обойдется?",'');
+      if ((typeof(a)) === 'string' && (typeof(a)!= null && (typeof(b)!= null)
+      && a!= '' && b!= '' && a.length < 50)) {
+        console.log("done");
+        appData.expenses[a] = b;
+      }else{
+       i--;
+    }
+    }
 }
-appData.moneyPerDay = appData.budget/30;
-alert("Ежедневный бюджет : " + appData.moneyPerDay);
+chooseExpenses();
 
+function detectDayBudget(){
+appData.moneyPerDay = (appData.budget/30).toFixed(1);//toFixed returns string
+alert("Ежедневный бюджет : " + appData.moneyPerDay);
+}
+detectDayBudget();
+function detectLevel(){
 if (appData.moneyPerDay < 100){
     console.log("Минимальный уровень достатка");
     
@@ -34,6 +49,7 @@ if (appData.moneyPerDay < 100){
     console.log('Высокий уровень достатка');
 } else {
     console.log('Произошла ошибка');
+}
 }
 
 /*let ind = 0;
@@ -59,4 +75,25 @@ do {
        i = -1;
      }
 }while (ind < 2);*/
-
+function checkSavings(){
+if (appData.savings == true){
+  let save = +prompt("Какова сумма накоплений?");
+  let percent = +prompt("Под какой процент?");
+  appData.monthIncome = save/100/12*percent;
+  alert("Доход в месяц с депозита: " + appData.monthIncome);
+}
+}
+checkSavings();
+function chooseOptExpenses(){
+  for (i=0;i<3;i++){
+    var c = prompt("Введите необязательную статью расходов в этом месяце или оставьте пустым",'');
+    var d = prompt("Введите сумму доп трат или оставьте пустым");
+     if(typeof(c) === 'string'  && d != null && c != null && c.length < 20){
+console.log("done for optexpense");
+appData.optionalExpenses [c] = d;
+     }else{
+      break;
+     }  
+  }
+}
+chooseOptExpenses();
